464e10e4235bb1c3d7bcb81a3cfb9067c7fe6770
kmhk.lu <kmhk.lu@aol.com>
2019-07-11 23:20:14 +0300

    Initial Commit

---
+++ VideoText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoText
+//
+//  Created by MacMaster on 7/11/19.
+//  Copyright © 2019 MacMaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
